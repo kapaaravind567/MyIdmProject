02afb8f18e37016b69f1d65610177fcd224d266b
kapaaravind567 <aravind.kapa567@gmail.com>
2020-10-20 12:21:21 +0530

    MyFile

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,5 @@
-mysql -u root -p'Xp1o0^Vf9w5#X' -h 192.168.1.69 -P 3306 -D ojasinv-it <<EOF
+mysql -u root -p'Xp1o0^Vf9w5#X' -h 192.168.2.145 -P 3306 -D ojasinv-it <<EOF
 ls
 show tables
-EOF 
+EOF
 
